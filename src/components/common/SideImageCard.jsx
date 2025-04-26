e4e3238be20fde15303fd4133a0d15438499a6f1
Vikas <iamvikasbareth@gmail.com>
2025-04-26 17:31:46 +0530

    custom reusable components added

---
+++ src/components/common/SideImageCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const SideImageCard = ({
+  card_title,
+  card_description,
+  card_image_url,
+  children,
+}) => {
+  return (
+    <div>
+      <div className="card card-side bg-base-100 shadow-sm">
+        <figure>
+          <img src={card_image_url} alt="Card Side Image" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{card_title}</h2>
+          <p>{card_description}</p>
+          {children && <div className="mt-4">{children}</div>}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideImageCard;
