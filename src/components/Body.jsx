9f532869d2dc137756c9c20f48691d605c8bb711
Vikas <iamvikasbareth@gmail.com>
2025-04-27 01:54:08 +0530

    body updated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,46 +1,7 @@
-import axios from "axios";
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Outlet, useNavigate } from "react-router-dom";
-import { APP_BASE_URL, GET_USER } from "../utils/constants";
-import { addUser, removeUser } from "../utils/userSlice";
-import Navbar from "./Navbar";
+import React from "react";
 
 const Body = () => {
-  const user = useSelector((state) => state.user);
-
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const fetchUser = async () => {
-    try {
-      const user = await axios.get(APP_BASE_URL + GET_USER, {
-        withCredentials: true,
-      });
-      if (user.status === 401) {
-        dispatch(removeUser());
-        navigate("/login");
-      }
-      dispatch(addUser(user.data));
-
-      return;
-    } catch (error) {
-      return navigate("/login");
-    }
-  };
-
-  useEffect(() => {
-    if (!user) {
-      fetchUser();
-    }
-  }, [user]);
-
-  return (
-    <div>
-      <Navbar />
-      <Outlet />
-    </div>
-  );
+  return <div>Body</div>;
 };
 
 export default Body;
